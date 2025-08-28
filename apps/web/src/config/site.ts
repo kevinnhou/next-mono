83f13f396cd020c4234b213e7e580b5464dc07aa
Kevin Hou <strawberrypi64@gmail.com>
2025-08-28 11:45:13 +1000

    chore: initialise monorepo

---
+++ apps/web/src/config/site.ts
@@ -0,0 +1,27 @@
+export const site = {
+  author: {
+    links: "",
+    name: "",
+    tag: "",
+  },
+  category: "",
+  datePublished: "",
+  description: "",
+  keywords: [],
+  links: {
+    repo: "",
+    url:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : `http://${process.env.VERCEL_PROJECT_PRODUCTION_URL || ""}`,
+  },
+  name: "mono",
+  operatingSystem: "",
+  rating: {
+    bestRating: 0,
+    ratingCount: 0,
+    ratingValue: 0,
+    worstRating: 0,
+  },
+  type: "",
+};
